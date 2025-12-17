e81b4c095c1a08f20ff849f73e8a21252402dc3b
Danilo Silva <danilo.silva@msn.com>
2025-12-17 11:13:00 -0300

    feat: configurar Tailwind v4 e atualizar layout inicial

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  imports: [],
+  templateUrl: './app.html'
 })
 export class App {
   protected readonly title = signal('meus-medicamentos');
